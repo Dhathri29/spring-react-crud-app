f461744e0967880241990f10981e10c8789fb8e3
dhathri <dhathrivupparapalli@gmail.com>
2021-09-12 23:13:00 +0530

    added Edit user functionality

--- src/main/java/com/springboot/controller/EmployeeController.java
+++ src/main/java/com/springboot/controller/EmployeeController.java
@@ -3,13 +3,17 @@ package com.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.exception.ResourceNotFoundException;
 import com.springboot.model.Employee;
 import com.springboot.repository.EmployeeRepository;
 
@@ -32,6 +36,29 @@ public class EmployeeController {
 	@PostMapping("/employees")
 	public Employee addEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
+	}
+	
+	//get Employee
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployee(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee Not found"));
+		return ResponseEntity.ok(employee);
+	}
+	
+	//edit employee
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee Not found"));
+		
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId());
+		
+		Employee updatedEmployee = employeeRepository.save(employee);
+		
+		return ResponseEntity.ok(updatedEmployee);
 	}
 	
 }
